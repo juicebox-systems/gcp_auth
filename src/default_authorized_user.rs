be7d866dad940cd95de53a116765bd7a8e1feaf2
Diego Ongaro <diego@juicebox.me>
2023-08-22 17:09:34 -0700

    Replace `dirs-next` with `home` dependency
    `home` is part of the Cargo repo.

--- src/default_authorized_user.rs
+++ src/default_authorized_user.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::sync::RwLock;
 
 use async_trait::async_trait;
+use home::home_dir;
 use hyper::body::Body;
 use hyper::{Method, Request};
 use serde::{Deserialize, Serialize};
@@ -24,7 +25,7 @@ impl ConfigDefaultCredentials {
 
     pub(crate) async fn new(client: &HyperClient) -> Result<Self, Error> {
         tracing::debug!("Loading user credentials file");
-        let mut home = dirs_next::home_dir().ok_or(Error::NoHomeDir)?;
+        let mut home = home_dir().ok_or(Error::NoHomeDir)?;
         home.push(Self::USER_CREDENTIALS_PATH);
 
         let file = fs::File::open(home).map_err(Error::UserProfilePath)?;
